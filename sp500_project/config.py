545824651b37a1b2367b52c13b9707e3afb157f6
Max <perkovsky.maxim@gmail.com>
2023-03-14 21:25:26 +0100

    Added classwork

---
+++ sp500_project/config.py
@@ -0,0 +1,2 @@
+DB_TYPE = "json"
+DB_FILE = "sp500.json"
--- sp500_project/data_access.py
+++ sp500_project/data_access.py
@@ -1,17 +1,67 @@
 import csv
+import json
+from abc import ABC, abstractmethod
 
 
-class RecordAlreadyExists(Exception):
-    ...
+class IStorage(ABC):
+    @abstractmethod
+    def get_all_records(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def record_exists(self, company_name):
+        raise NotImplementedError
 
 
-def get_all_records():
-    with open("sp500.csv") as db_file:
-        return list(csv.DictReader(db_file))
+class CSVStorage(IStorage):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def file_name(self, value):
+        if value.split(".")[1] != "csv":
+            raise ValueError("Invalid file extension.")
+
+        self._file_name = value
+
+    def get_all_records(self):
+        with open(self.file_name) as db_file:
+            return list(csv.DictReader(db_file))
+
+    def record_exists(self, company_name):
+        with open(self.file_name) as db_file:
+            for row in csv.DictReader(db_file):
+                if company_name == row["Name"]:
+                    return True
+        return False
 
 
-def record_exist(company_name):
-    with open("sp500.csv") as db_file:
-        for row in csv.DictReader(db_file):
-            if company_name == row["Name"]:
-                raise RecordAlreadyExists("Company already exists.")
+class JSONStorage(IStorage):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def file_name(self, value):
+        if value.split(".")[1] != "json":
+            raise ValueError("Invalid file extension.")
+
+        self._file_name = value
+
+    def get_all_records(self):
+        with open(self.file_name) as db_file:
+            return json.load(db_file)
+
+    def record_exists(self, company_name):
+        with open(self.file_name) as db_file:
+            for row in json.load(db_file):
+                if company_name == row["Name"]:
+                    return True
+        return False
